7f7e15258bc6d5d9382b2137a770cddda9597787
Yannick Severo <severoyannick@gmail.com>
2021-09-16 19:27:16 +0200

    Bonus OK

--- js/exos.js
+++ js/exos.js
@@ -5,33 +5,39 @@
  * Et écris ton code ici même !
  */
 
- /* Exo 1 */
- // Ton code ici...
+/* Exo 1 */
+// Ton code ici...
 
- var question1 = 'Quelle mer borde la ville de Sébastopol ?';
- var solution1 = 'la mer Noire';
+var question1 = "Quelle mer borde la ville de Sébastopol ?";
+var solution1 = "la mer Noire";
 
- /* Exo 2 */
- // Ton code ici...
+/* Exo 2 */
+// Ton code ici...
 
- var reponse1 = prompt(question1);
+var result = 0;
 
- if(reponse1 === solution1) {
-     alert("Gagné 🎉");
- } else {
-     alert("Perdu 🧨");
- }
+var reponse1 = prompt(question1);
 
- /* Exo 3 */
- // Ton code ici...
+if (reponse1 === solution1) {
+  result++;
+  alert("Gagné 🎉");
+} else {
+  alert("Perdu 🧨");
+}
 
- var question2 = 'Quel est l\'âge du capitaine ?';
- var solution2 = 63;
+/* Exo 3 */
+// Ton code ici...
 
- var reponse2 = prompt(question2);
+var question2 = "Quel est l'âge du capitaine ?";
+var solution2 = 63;
 
- if(reponse2 == solution2) {
-     alert("Gagné 🎉");
- } else {
-     alert("Perdu 🧨");
- }
+var reponse2 = prompt(question2);
+
+if (reponse2 == solution2) {
+  result++;
+  alert("Gagné 🎉");
+} else {
+  alert("Perdu 🧨");
+}
+
+alert(`Vous avez ${result} bonnes réponses`);
